72836f551b82c59c9b1295f283fc4a377608a582
Camillo <ballandt@proton.me>
2023-05-05 13:55:21 +0200

    Problem comment

--- src/phyreg/core/weights.py
+++ src/phyreg/core/weights.py
@@ -2,6 +2,7 @@ from numpy import array, max, mean
 
 
 def reversed_err(p, ex_f):
-    """Weight is the difference of the maximal error and the specific error."""
+    """Weight is the difference of the maximal error and the specific error.
+    Problem: Least precise value gets weight 0."""
     err = array([abs(ex_f(ele[0]) - ele[1]) for ele in p])
     return max(err) - err
